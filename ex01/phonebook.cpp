e1eea456d394b3fc3e0297aecddcf91f033e9b00
Fernando De Jesus Lucart <fde-jesu@c1r2s3.42porto.com>
2025-01-10 16:38:17 +0000

    c00

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: fde-jesu <fde-jesu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 02:37:38 by fde-jesu          #+#    #+#             */
-/*   Updated: 2025/01/10 04:07:16 by fde-jesu         ###   ########.fr       */
+/*   Updated: 2025/01/10 16:28:39 by fde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,34 @@ void fill_phonebook(Phonebook *phone)
 {
 	std::string cmdl;
 	
+	int x;
 	int index = 0;
 	
 		std::cout << "> valid commands are: 'EXIT'; 'ADD'; 'SEARCH';" << std::endl;
 		std::cout << "> otherwise, it will be ignored:" << std::endl;
 	do
 	{
-		std::cout << ">";
+		std::cout << "$>";
 		getline(std::cin, cmdl);
 		if (std::cin.eof())
 			break;
 		if (cmdl.compare("SEARCH") == 0)
 		{
-			int x;
 			do
 			{
 				std::cout << "enter page: ";
 				std::cin >> x;
-				if (x > MAX_C)
-					std::cout << "error: page not avaliable\ntip  :please enter page between 0 and 8" << std::endl;	
+				if (!isdigit(x))
+				{
+					std::cout << "error\n" << std::endl;
+					
+				}
+				else if (x > MAX_C){
+					std::cout << "error: page not avaliable\n";
+					std::cout <<"tip  :please enter page between 0 and 8" << std::endl;
+				}
 			} while (x > MAX_C);
-			
-			std::cout << "__________ __________ __________ __________" << std::endl;
-			phone->contact[x].display_info(phone->contact[x].first_name);
-			phone->contact[x].display_info(phone->contact[x].last_name);
-			phone->contact[x].display_info(phone->contact[x].nickname);
-			phone->contact[x].display_info(phone->contact[x].phone_nbr);
-			std::cin.ignore();
-			std::cout << std::endl;
+			phone->contact[x].display_user();
 		}
 		else if (cmdl.compare("ADD") == 0)
 		{
@@ -52,11 +52,12 @@ void fill_phonebook(Phonebook *phone)
 			phone->contact[index++].get_info();
 		}
 		else if (cmdl.compare("EXIT") == 0)
-		{
 			break ;
-		}
+		else if (cmdl.empty())
+			continue;
 		else
 		{
+			//std::cout << "." << cmdl << "." <<  std::endl;
 			std::cout << "invalid syntax:" << std::endl;
 		}
 	}while(1);
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: fde-jesu <fde-jesu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 02:37:30 by fde-jesu          #+#    #+#             */
-/*   Updated: 2025/01/10 02:36:50 by fde-jesu         ###   ########.fr       */
+/*   Updated: 2025/01/10 16:22:44 by fde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,28 +20,16 @@
 
 class Contact{
 	public:
-		std::string first_name;
-		std::string last_name;
-		std::string nickname;
-		std::string phone_nbr;
-
+		
 	void get_info()
 	{
-		std::cout << "enter first name   : ";
-		getline(std::cin >> std::ws, first_name);
-		
-		std::cout << "enter last name    : ";
-		getline(std::cin >> std::ws, last_name);
-		
-		std::cout << "enter nick name    : ";
-		getline(std::cin >> std::ws, nickname);
-		
-		std::cout << "enter phone nbr    : ";
- 		getline(std::cin >> std::ws, phone_nbr);		
-
-		std::cout << "enter  dark secret: ";
-		getline(std::cin >> std::ws, dark_secret);		
+		get_info("enter first name  : ", first_name);
+		get_info("enter last_name   : ", last_name);
+		get_info("enter nickname    : ", nickname);
+		get_info("enter phone number: ", phone_nbr);
+		get_info("enter dark secret : ", dark_secret);
 	}
+	
 	void display_info(std::string text)
 	{
 		if(text.length() > 10)
@@ -49,24 +37,41 @@ class Contact{
 			text.resize(9);
 			text.append(".");
 		}
-		std::cout << std::setfill(' ') << std::setw(10) << text << "|";
-	//if(newline)
-	//	std::cout << std::setfill(' ') << std::setw(10) << text << std::endl;
-	//else
-
+		if (text == "\n")
+			std::cout << std::setw(10) << "" << "|";
+		else
+			std::cout << std::setw(10) << text << "|";
 	}
-	std::string get_dark_secret(void)
+	void display_user()
 	{
-		return (dark_secret);
+			std::cout << "__________ __________ __________ __________" << std::endl;
+			display_info(first_name);
+			display_info(last_name); // display tem que ser um metodo e 
+			display_info(nickname);
+			display_info(phone_nbr);
+			std::cin.ignore();
+			std::cout << std::endl;
 	}
 	private:
+		std::string first_name;
+		std::string last_name;
+		std::string nickname;
+		std::string phone_nbr;
 		std::string dark_secret;
+		
+		void get_info(std::string question, std::string &var)
+		{
+			std::cout << question ;
+			getline(std::cin /* >> std::ws */, var);
+			if (std::cin.eof())
+				exit(1);
+		}
+
 };
 
 class Phonebook{
 	public:
 		Contact contact[MAX_C];
 };
-
 
 #endif
